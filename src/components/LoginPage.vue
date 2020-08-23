33ba68510b3f8b3e35191e8294be75ff677444aa
Merve Yıldız <merve.yildiz41@hotmail.com>
2020-08-23 23:37:27 +0300

    css düzenlendi

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="login" class="mt-5">
     <div class="m-5">
-      <div class="col-lg-6  col-md-8 col-sm-10  centered ">
-        <h4 class="text-center ">LOGIN</h4>
+      <div class="col-lg-8  col-md-8 col-sm-10  centered my-box ">
+        <h4 class="text-center text-muted">LOGIN</h4>
         <input
           type="text"
           placeholder="Username"
@@ -29,25 +29,30 @@ export default {
   data: () => ({
     username: '',
     password: '',
-    isLogin:false,
+    isLogin: false,
   }),
-  methods:{
-    login(){
-      if(this.username.length!=0 && this.password.length!=0 ){
-        this.username= '',
-         this.password= '',
-       this.$emit('LoginPage::isLogin', {isLogin: true})
+  methods: {
+    login() {
+      if (this.username.length != 0 && this.password.length != 0) {
+        (this.username = ''),
+          (this.password = ''),
+          this.$emit('LoginPage::isLogin', { isLogin: true });
+      } else {
+        alert('Lütfen boş bırakmayınız');
+        this.isLogin = false;
       }
-      else{
-        alert("Lürfen boş bırakmayınız");
-         this.isLogin=false;
-      }
-    }
-  }
+    },
+  },
 };
 </script>
 <style>
-.btn{
-  font-size:20px;
+.btn {
+  font-size: 20px;
+}
+.my-box {
+  border: 1px solid lightblue;
+  padding: 30px 20px;
+  border-radius: 10px;
+  box-shadow: 2px 2px #c2eafa;
 }
 </style>
